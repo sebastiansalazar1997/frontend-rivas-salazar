11f4dc32deca0a7c48536ccb0720df59c647c961
sebastiansalazar1997 <isb.salazar@yavirac.edu.ec>
2021-08-02 16:34:33 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectComponent } from './project/project.component';
+import { AuthorComponent } from './author/author.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+
+  {
+    path:'',
+    component:MainComponent
+  },
+  {
+    path:'projects',
+    component:ProjectComponent
+  },
+  {
+    path:'authors',
+    component:AuthorComponent
+    
+  },
+  {
+    path:'not-found',
+    component:NotFoundComponent
+    
+  },
+  {
+    path:'**',
+    redirectTo:'/not-found'
+    
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {ButtonModule} from 'primeng/button';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
+import { AuthorComponent } from './author/author.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MainComponent } from './main/main.component';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
+import { UnderMaintenanceComponent } from './under-maintenance/under-maintenance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    AuthorComponent,
+    NotFoundComponent,
+    MainComponent,
+    AccessDeniedComponent,
+    UnderMaintenanceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
